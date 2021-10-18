c527d18e58c494e587266be25f8e6c79bfd2991c
Mike <1079537183@qq.com>
2021-10-18 17:28:07 +0800

    搭建html

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="bannner">
+      <h1>轱辘UI</h1>
+      <h2>一个厉害的 UI 框架</h2>
+      <p class="actions">
+        <a>GitHub</a>
+        <a>开始</a>
+      </p>
+    </div>
+  </div>
+</template>
