c43c17eec7b2ea432bbb37c8039bf94ccb3aca4b
Mike <1079537183@qq.com>
2021-10-18 17:49:18 +0800

    Topnav组件封装完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
+    <Topnav />
     <div class="bannner">
       <h1>轱辘UI</h1>
       <h2>一个厉害的 UI 框架</h2>
@@ -14,3 +11,14 @@
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import Topnav from '../components/Topnav.vue'
+export default {
+  components: {Topnav}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
