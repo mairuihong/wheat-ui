0311a425d2dff27f394748acc88c38d600e03c55
Mike <1079537183@qq.com>
2021-10-18 16:18:30 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <frank/>
+</template>
+
+<script>
+import frank from "./components/frank.vue";
+
+export default {
+  name: "App",
+  components: {
+    frank,
+  },
+};
+</script>
