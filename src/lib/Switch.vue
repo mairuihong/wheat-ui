cab1882b5bbcc9d910486c7261fb840333eee0c3
Mike <1079537183@qq.com>
2021-10-20 16:44:24 +0800

    Switch css 2

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -27,7 +27,7 @@ export default {
     height: $h;
     width: $h*2;
     border: none;
-    background: gray;
+    background: #bfbfbf;
     border-radius: $h/2;
     position: relative;
   }
@@ -39,15 +39,21 @@ export default {
     width: $h2;
     background:white;
     border-radius: $h2 / 2;
-    transition: left 250ms;
+    transition: all 250ms;
   } 
   button.checked {
-    background: blue;
+    background: #1890ff;
   }
   button.checked > span {
     left: calc(100% - #{$h2} - 2px)
   }
   button:focus{
     outline: none;
+  }
+  button:active{
+    > span {width: $h2 + 4px;}
+  }
+  button.checked:active{
+    > span {width: $h2 + 4px; margin-left: -4px;}
   }
 </style>
