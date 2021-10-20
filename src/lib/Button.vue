e233ee22440d7a081c95676c064d8e9ebf4ee536
Mike <1079537183@qq.com>
2021-10-20 20:07:46 +0800

    支持 theme 和 size，写 size css

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,16 +1,31 @@
 <template>
-  <button class="gulu-button" :class="{[`gulu-theme-${theme}`]: theme}">
+  <button class="gulu-button" :class="classes">
     <slot />
   </button>
 </template>
 
 <script lang='ts'>
+import { computed } from '@vue/reactivity'
 export default {
   props: {
     theme: {
       type: String,
       default: 'button'
-    } 
+    },
+    size: {
+      type: String,
+      default: 'normal'
+    }
+  },
+  setup(props) {
+    const { theme, size } = props
+    const classes = computed(()=>{
+      return {
+        [`gulu-theme-${theme}`]: theme,
+        [`gulu-size-${size}`]: size
+      }
+    })
+    return {classes}
   }
 }
 </script>
@@ -48,6 +63,34 @@ export default {
     }
     &::-moz-focus-inner {
       border: 0;
+    }
+    &.gulu-theme-link{
+      border-color: transparent;
+      box-shadow: none;
+      color: $blue;
+      &:hover,&:focus{
+      color: lighten($blue, 10%);
+      }
+    }
+    &.gulu-theme-text{
+      border-color: transparent;
+      box-shadow: none;
+      color: inherit;
+      &:hover,&:focus{
+        background: darken(white, 5%);;
+      }
+    }
+    &.gulu-theme-button{
+      &.gulu-size-big{
+        font-size: 24px;
+        height: 48px;
+        padding: 0 16px
+      }
+      &.gulu-size-small{
+        font-size: 12px;
+        height: 20px;
+        padding: 0 4px;
+      }
     }
   }
 </style>
