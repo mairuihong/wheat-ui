991c896bbc1a172726a67c330c7fda5551f71941
Mike <1079537183@qq.com>
2021-10-20 19:40:48 +0800

    CSS影响最小原则

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import "./lib/gulu.scss"
 import './index.scss'
+import { createApp } from 'vue'
+import App from "./App.vue"
 import { router } from './router'
 
 const app =  createApp(App)
