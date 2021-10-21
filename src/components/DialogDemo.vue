c289c41a162131748387ad632a3c443f39e5cf9b
Mike <1079537183@qq.com>
2021-10-21 09:04:57 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    Dialog 组件
+    Dialog 示例
   </div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
+
+<script lang='ts'>
+import Dialog from '../lib/Dialog.vue'
+export default {
+  components: {
+    Dialog
+  }
+}
+</script>
