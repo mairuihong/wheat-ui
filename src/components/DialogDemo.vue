df993af2ab6d7394b85f2eab3d2c18cdfd308203
Mike <1079537183@qq.com>
2021-10-21 09:45:26 +0800

    Dialog 支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -4,7 +4,7 @@
   </div>
   <h1>示例1</h1>
   <Button @click="toggle">toggle</Button>
-  <Dialog :visible="x"></Dialog>
+  <Dialog v-model:visible="x"  :closeOnClickOverlay="false" :ok="f1" :cancel="f2"></Dialog>
 </template>
 
 <script lang='ts'>
@@ -21,7 +21,12 @@ export default {
     const toggle = (()=>{
       x.value = !x.value
     })
-    return {x, toggle}
+    const f1 = () => {
+      return false
+    }
+    const f2 = () => {
+    }
+    return {x, toggle, f1, f2}
   }
 }
 </script>
