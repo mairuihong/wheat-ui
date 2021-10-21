56156ccbabdfe37f002f2f3fb39b4ad06c75840c
Mike <1079537183@qq.com>
2021-10-21 11:25:35 +0800

    创建 Tabs 和 Tab 组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    Tabs 组件
+    Tabs 示例
   </div>
+  <h1>示例1</h1>
+  <Tabs>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
+
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+export default {
+  components: {
+    Tabs,
+    Tab
+  }
+}
+</script>
