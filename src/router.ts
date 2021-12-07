4d465422bc6a41f2d2a3e9a0da130d94a6683ae7
Mike <1079537183@qq.com>
2021-12-07 16:48:02 +0800

    修改内容

--- src/router.ts
+++ src/router.ts
@@ -12,7 +12,7 @@ import getStarted from './markdown/get-started.md'
 import install from './markdown/install.md'
 
 const history = createWebHashHistory()
-const md = string => h(Markdown, { conntent: string, key: string })
+const md = string => h(Markdown, { content: string, key: string })
 export const router = createRouter({
   history: history,
   routes: [
