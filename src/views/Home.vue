24b31737431809aead3cc1f1f025437eed2f6f16
Mike <1079537183@qq.com>
2021-12-07 16:56:54 +0800

    修改 Home 样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,89 +1,161 @@
 <template>
-  <div>
+  <div class="homepage">
     <div class="topnavAndBanner">
-      <Topnav />
       <div class="banner">
-        <h1>轱辘UI</h1>
-        <h2>一个厉害的 UI 框架</h2>
-        <p class="actions">
-          <a href="https://github.com">GitHub</a>
-          <router-link to="/doc">开始</router-link>
-        </p>
+        <div class="logo-png">
+          <img src="../assets/wheat.png" alt="wheat-UI">
+        </div>
+        <div class="details">
+          <h1>Wheat UI</h1>
+          <h2>一个厉害的 UI 框架</h2>
+          <p class="actions">
+            <a href="https://github.com">GitHub</a>
+            <router-link to="/doc">Get Started</router-link>
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="features">
-    <ul>
-      <li>
-        <svg>
-          <use xlink:href="#icon-vue"></use>
-        </svg>
-        <h3>基于 Vue 3</h3>
-        <p>骄傲地使用了 Vue 3 Composition API</p>
-      </li>
-      <li>
-        <svg>
-          <use xlink:href="#icon-ts"></use>
-        </svg>
-        <h3>基于 TypeScript</h3>
-        <p>源代码采用 TypeScript 书写</p>
-      </li>
-      <li>
-        <svg>
-          <use xlink:href="#icon-lights"></use>
-        </svg>
-        <h3>代码易读</h3>
-        <p>每个组件的源代码都极其简洁</p>
-      </li>
-    </ul>
+    <div class="features">
+      <ul>
+        <li>
+          <svg>
+            <use xlink:href="#icon-vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>使用了 Vue 3 Composition API</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 TypeScript</h3>
+          <p>源代码采用 TypeScript 书写</p>
+        </li>
+        <li>
+          <svg>
+            <use xlink:href="#icon-lights"></use>
+          </svg>
+          <h3>代码易读</h3>
+          <p>每个组件的源代码都极其简洁</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import Topnav from "../components/Topnav.vue";
+import Topnav from '../components/Topnav.vue';
+
 export default {
-  components: { Topnav },
+  components: {Topnav},
 };
 </script>
 
 <style lang="scss" scoped>
-$green: #02bcb0;
-$border-radius: 4px;
-$color: #28d1c9;
-
-.topnavAndBanner {
-  background: linear-gradient(
-    145deg,
-    rgba(227, 255, 253, 1) 0%,
-    rgba(183, 233, 230, 1) 100%
-  );
-  clip-path: ellipse(80% 60% at 50% 40%);
+.homepage {
+  width: 100%;
+  height: 100%;
+  background-color: #FBAB7E;
+  background-image: linear-gradient(45deg, #FBAB7E 0%, #F7CE68 100%);
 }
-.banner {
-  color: $color;
-  padding: 100px 0;
+.topnavAndBanner {
   display: flex;
   justify-content: center;
+  align-content: flex-start;
+}
+.banner {
+  background: rgba(255, 255, 255, 0.6);
+  color: #ff9933;
+  margin: 100px 150px;
+  padding: 100px 0;
+  width: 70vw;
+  height: 60vh;
+  max-width: 1000px;
+  border-radius: 30px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
   align-items: center;
-  flex-direction: column;
-  > .actions {
+  h1 {
+    font-weight: 700;
+    font-size: 48px;
+  }
+  h2 {
+    font-size: 20px;
+  }
+  h1, h2 {
+    text-align: center;
+  }
+  > .logo-png {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 24px;
+    > img {
+      width: 80%;
+      height: auto;
+    }
+  }
+  .actions {
+    margin-top: 80px;
     padding: 8px 0;
     a {
-      margin: 0 8px;
-      background: $green;
+      margin: 4px 8px;
+      background: #8dcb76;
       color: white;
       display: inline-block;
       padding: 8px 24px;
-      border-radius: $border-radius;
+      border-radius: 4px;
       &:hover {
         text-decoration: none;
       }
+    }
+  }
+  @media (max-width: 1000px) {
+    h1 {
+      font-size: 32px;
+    }
+    > .logo-png {
+      margin-right: 0px;
+      > img {
+        width: 75%;
+        height: auto;
+      }
+    }
+  }
+  @media (max-width: 800px) {
+    margin: 100px 24px;
+    width: 100vw;
+    > .logo-png {
+      > img {
+        width: 70%;
+        height: auto;
+      }
+    }
+  }
+  @media (max-width: 500px) {
+    margin: 20px 16px;
+    padding: 40px;
+    height: auto;
+    flex-direction: column;
+    > .logo-png {
+      margin-bottom: 12px;
+      > img {
+        width: 60%;
+        height: auto;
+      }
+    }
+    .actions {
+      margin-top: 12px;
     }
   }
 }
 .features {
   margin: 64px auto;
   padding: 0 16px;
+  @media (max-width: 500px) {
+    margin: 12px auto;
+  }
   @media (min-width: 800px) {
     width: 800px;
     > ul {
