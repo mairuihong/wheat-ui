706601869ec46ed5ef64e6a7892b683e09069e42
Mike <1079537183@qq.com>
2021-12-08 21:21:53 +0800

    修改项目名字

--- rollup.config.js
+++ rollup.config.js
@@ -10,8 +10,8 @@ export default {
     globals: {
       vue: 'Vue'
     },
-    name: 'Gulu',
-    file: 'dist/lib/gulu.js',
+    name: 'wheat',
+    file: 'dist/lib/wheat.js',
     format: 'umd',
     plugins: [terser()]
   },
